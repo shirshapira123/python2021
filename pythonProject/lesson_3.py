b871e45d5499aafbe8a1330583fb8ca743e4a567
shir <b33860@ganyavne.ort.org.il>
2020-09-26 12:49:16 +0300

    lesson 3

--- pythonProject/lesson_3.py
+++ pythonProject/lesson_3.py
@@ -1,3 +1,7 @@
+import math
+import random
+
+
 def two():
     import random
     lst = []
@@ -42,5 +46,75 @@ def four():
         print(i[0], i)
 
 
+def five():
+    names = []
+    name = input("enter name: ")
+    names_counter = 0
+    while name != 'exit':
+        names.append(name)
+        names_counter += 1
+        name = input("enter name: ")
+    print(names_counter)
+
+
+def six():
+    female = []
+    name = input("enter name: ")
+    sex_type = input("enter your sex type:")
+    female_counter = 0
+    while name != 'exit':
+        if sex_type == 'F':
+            female.append(name)
+            female_counter += 1
+        name = input("enter name: ")
+        sex_type = input("enter your sex type:")
+    print(female_counter)
+
+
+def seven():
+    x_lst = []
+    y_lst = []
+    for i in range(-100, 100):
+        num = i/10
+        x_lst.append(num)
+        y_lst.append(0.5*num+2.5)
+    print(x_lst)
+    print(y_lst)
+
+
+def eight():
+    x_randoms = []
+    y_randoms = []
+    min_distance = math.sqrt(101**2+102**2)
+    point = 0
+    for i in range(10):
+        x_randoms.append(random.randint(1, 101))
+        y_randoms.append(random.randint(1, 101))
+        print(f"({x_randoms[i]},{y_randoms[i]})")
+        distance = math.sqrt(x_randoms[i]**2 + y_randoms[i]**2)
+        min_distance = min(min_distance, distance)
+        if distance == min_distance:
+            point = i
+    print(f"({x_randoms[point]},{y_randoms[point]}), distance = {min_distance}")
+
+
+def nine():
+    x_randoms = []
+    y_randoms = []
+    z_randoms = []
+    min_distance = math.sqrt((101-50)**2+(101-50)**2+(102-50)**2)
+    point = 0
+    for i in range(10):
+        x_randoms.append(random.randint(1, 101))
+        y_randoms.append(random.randint(1, 101))
+        z_randoms.append(random.randint(1, 101))
+        print(f"({x_randoms[i]},{y_randoms[i]},{z_randoms[i]})")
+        distance = math.sqrt((x_randoms[i]-50)**2+(y_randoms[i]-50)**2+(z_randoms[i]-50)**2)
+        min_distance = min(min_distance, distance)
+        if distance == min_distance:
+            point = i
+    print(f"({x_randoms[point]},{y_randoms[point]},{z_randoms[point]}), distance = {min_distance}")
+
+
 if __name__ == '__main__':
-    four()
+    nine()
